3f1db608a700720a7d13f53f2a24dd066bf34479
zhenxun815 <guoyiheng89@gmail.com>
2018-06-07 19:22:26 +0800

    添加点击事件和拖动事件

--- src/main/java/com/tqhy/client/controller/Controller.java
+++ src/main/java/com/tqhy/client/controller/Controller.java
@@ -1,15 +1,48 @@
 package com.tqhy.client.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class Controller {
+    private double xOffset = 0;
+    private double yOffset = 0;
     @FXML
-    private Button bt_test;
+    GridPane gridPane;
 
-    public void click(MouseEvent event) {
+    public void press(MouseEvent event) {
+        event.consume();
+        MouseButton button = event.getButton();
+        if (MouseButton.SECONDARY == button) {
+            System.out.println(button.name() + "....");
+        } else if (MouseButton.PRIMARY.equals(button)) {
+            if (MouseEvent.MOUSE_PRESSED == event.getEventType()) {
+                System.out.println("left press...");
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        }
+    }
 
-        System.out.println(event.getButton().name() + "....");
+    public void drag(MouseEvent event) {
+        event.consume();
+        MouseButton button = event.getButton();
+        if (MouseButton.SECONDARY == button) {
+            System.out.println(button.name() + "....");
+        } else if (MouseButton.PRIMARY.equals(button)) {
+            if (MouseEvent.MOUSE_DRAGGED == event.getEventType()) {
+                System.out.println("left drag..");
+                Stage stage = (Stage) gridPane.getScene().getWindow();
+                stage.setX(event.getScreenX() - xOffset);
+                if (event.getScreenY() - yOffset < 0) {
+                    stage.setY(0);
+                } else {
+                    stage.setY(event.getScreenY() - yOffset);
+                }
+            }
+        }
     }
 }
