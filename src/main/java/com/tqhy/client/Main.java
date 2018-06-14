b46fe8ee62431501dc5e745e3fb9d71a655711f8
zhenxun815 <guoyiheng89@gmail.com>
2018-06-14 10:57:44 +0800

    新增轮询后台方法doNetWork()

--- src/main/java/com/tqhy/client/Main.java
+++ src/main/java/com/tqhy/client/Main.java
@@ -1,6 +1,11 @@
 package com.tqhy.client;
 
+import com.tqhy.client.controller.ListViewDialogController;
+import com.tqhy.client.network.Network;
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,10 +13,13 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Platform.setImplicitExit(false);
         Parent root = FXMLLoader.load(getClass().getResource("/float/float.fxml"));
         primaryStage.setAlwaysOnTop(true);
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/deploy/package/windows/client.png")));
@@ -19,6 +27,27 @@ public class Main extends Application {
         primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        doNetWork();
+    }
+
+    /**
+     * 通过轮询请求后台,获取当前查看影像信息
+     */
+    private void doNetWork() {
+        Network.getAiResultApi()
+                .getTest()
+                .repeatWhen(objectObservable ->
+                        objectObservable.flatMap(o ->
+                                Observable.just(1).delay(3000, TimeUnit.MILLISECONDS)))
+                .observeOn(Schedulers.trampoline())
+                .subscribe(testMsg ->
+                        Platform.runLater(() -> {
+                            System.out.println(testMsg);
+                            ListViewDialogController listViewDialogController = new ListViewDialogController(testMsg);
+                            listViewDialogController.show();
+                        })
+                );
     }
 
 
