04998318f040c54626b4862b38077aea526ab4ec
zhenxun815 <guoyiheng89@gmail.com>
2018-06-14 11:19:31 +0800

    1.类名修改:DialogController -> ListViewDialogController 2.修改测试条目显示内容

--- src/main/java/com/tqhy/client/controller/ListViewDialogController.java
+++ src/main/java/com/tqhy/client/controller/ListViewDialogController.java
@@ -1,5 +1,6 @@
 package com.tqhy.client.controller;
 
+import com.tqhy.client.model.bean.TestMsg;
 import com.tqhy.client.utils.FxmlUtils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -22,7 +23,7 @@ import java.util.List;
  * @create 2018/6/8
  * @since 1.0.0
  */
-public class DialogController extends DialogPane {
+public class ListViewDialogController extends DialogPane {
 
     @FXML
     private ListView dialog_lv;
@@ -30,12 +31,14 @@ public class DialogController extends DialogPane {
     private ObservableList observableList;
     private Dialog<String> dialog;
     private Stage owner;
+    private TestMsg msg;
 
-    public DialogController() {
-        FxmlUtils.load("/dialog/dialog.fxml", this);
+    public ListViewDialogController(TestMsg msg) {
+        this.msg = msg;
+        FxmlUtils.load("/dialog/lvdialog/lv_dialog.fxml", this);
         initDialog();
         initListViewItems();
-        System.out.println("construct dialog controller");
+        //System.out.println("construct dialog controller");
     }
 
     private void initDialog() {
@@ -49,17 +52,20 @@ public class DialogController extends DialogPane {
         dialog.initOwner(owner);
     }
 
+    /**
+     * 填充ListView中每条item显示内容
+     */
     private void initListViewItems() {
         observableList = FXCollections.observableArrayList();
-        datas = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            datas.add("item content " + i);
-        }
+        datas = msg.data.source;
         observableList.addAll(datas);
         dialog_lv.setItems(observableList);
         dialog_lv.setCellFactory(lv -> new ListViewItemController());
     }
 
+    /**
+     * 显示弹窗.默认在桌面右下角弹出.
+     */
     public void show() {
         Screen primary = Screen.getPrimary();
         Rectangle2D visualBounds = primary.getVisualBounds();
@@ -69,13 +75,13 @@ public class DialogController extends DialogPane {
         double width = visualBounds.getWidth();
         double minX = visualBounds.getMinX();
         double minY = visualBounds.getMinY();
-        System.out.println("max x: " + maxX + " max y: " + maxY);
-        System.out.println("min x: " + minX + " min y: " + minY);
-        System.out.println("width: " + width + " height: " + height);
+        // System.out.println("max x: " + maxX + " max y: " + maxY);
+        // System.out.println("min x: " + minX + " min y: " + minY);
+        // System.out.println("width: " + width + " height: " + height);
         dialog.setX(maxX - this.getWidth());
 
         dialog.setY(maxY - this.getHeight());
-        System.out.println("x: " + dialog.getX() + " y: " + dialog.getY());
+        // System.out.println("x: " + dialog.getX() + " y: " + dialog.getY());
         dialog.showAndWait();
     }
 
