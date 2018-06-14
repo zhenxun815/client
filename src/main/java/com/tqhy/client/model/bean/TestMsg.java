dc0d986df5a0688bbd88dc4b22b4de22f4a38c19
zhenxun815 <guoyiheng89@gmail.com>
2018-06-14 19:07:24 +0800

    测试用bean

--- src/main/java/com/tqhy/client/model/bean/TestMsg.java
+++ src/main/java/com/tqhy/client/model/bean/TestMsg.java
@@ -8,9 +8,28 @@ import com.google.gson.annotations.SerializedName;
  * @since 1.0.0
  */
 public class TestMsg {
- public @SerializedName("data") Data data;
- public @SerializedName("desc") String desc;
- public @SerializedName("status") String status;
+    public @SerializedName("data")
+    Data data;
+    public @SerializedName("desc")
+    String desc;
+    public @SerializedName("status")
+    String status;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (null == desc) return false;
+        TestMsg testMsg = (TestMsg) o;
+        boolean b = desc.equals(testMsg.desc);
+        this.desc = testMsg.desc;
+        return b;
+    }
+
+    @Override
+    public int hashCode() {
+        return desc.hashCode();
+    }
 
     @Override
     public String toString() {
