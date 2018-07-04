217c45be207ee8068c4fcfbd79f81b8cc32dcae8
zhenxun815 <guoyiheng89@gmail.com>
2018-07-04 11:23:24 +0800

    格式化代码

--- src/test/java/TestLog.java
+++ src/test/java/TestLog.java
@@ -1,13 +1,5 @@
-import org.apache.log4j.Appender;
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Layout;
 import org.apache.log4j.Logger;
-import org.apache.log4j.spi.ErrorHandler;
-import org.apache.log4j.spi.Filter;
-import org.apache.log4j.spi.LoggingEvent;
 import org.junit.Test;
-import sun.rmi.runtime.Log;
-
 
 
 /**
@@ -17,7 +9,7 @@ import sun.rmi.runtime.Log;
  */
 public class TestLog {
     @Test
-    public void testLog(){
+    public void testLog() {
         Logger stdout = Logger.getLogger(TestLog.class);
         stdout.info("test,,");
 
