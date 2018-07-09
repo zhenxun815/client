ef3cfc3ca08ab00b488960fb9b1964a6b1bd0c69
zhenxun815 <guoyiheng89@gmail.com>
2018-07-09 10:28:19 +0800

    1.添加注释 2.方法名修改:requestAiHelper() -> getAiWarning();postAiWarning() -> postAiWarningBack()

--- src/main/java/com/tqhy/client/network/api/AiHelperApi.java
+++ src/main/java/com/tqhy/client/network/api/AiHelperApi.java
@@ -1,6 +1,6 @@
 package com.tqhy.client.network.api;
 
-import com.tqhy.client.model.bean.ClientMsg;
+import com.tqhy.client.model.ClientMsg;
 import io.reactivex.Observable;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -15,15 +15,43 @@ import retrofit2.http.Path;
  * @since 1.0.0
  */
 public interface AiHelperApi {
+    /**
+     * 获取ai提示弹框提示内容
+     * @param key
+     * @return
+     */
     @GET("warning/{key}")
-    Observable<ResponseBody> requestAiHelper(@Path("key") String key);
+    Observable<ResponseBody> getAiWarning(@Path("key") String key);
 
+    /**
+     * 弹出提示信息后向后台返回否接收到弹窗信息及弹框相关操作
+     * @param warningBack
+     * @return
+     */
     @POST("warningback")
-    Observable<ResponseBody> postAiWarning(@Body ApiBean<ClientMsg> warningBack);
+    Observable<ResponseBody> postAiWarningBack(@Body ApiBean<ClientMsg> warningBack);
 
+    /**
+     * 向后台发送用户操作历史
+     * @param date
+     * @return
+     */
     @POST("confirm")
     Observable<ResponseBody> postHistory(@Body ApiBean<ClientMsg> date);
 
+    /**
+     * 获取当前aiDrId
+     * @param key
+     * @return
+     */
+    @GET("aiDrId/{key}")
+    Observable<ResponseBody> getAiDrId(@Path("key") String key);
+
+    /**
+     * 测试
+     * @param key
+     * @return
+     */
     @GET("warning/{key}")
     Call<ResponseBody> getTest(@Path("key") String key);
 }
