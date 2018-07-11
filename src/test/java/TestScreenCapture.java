941665b58e0dfc55161ac89c8424870beed2ec83
zhenxun815 <guoyiheng89@gmail.com>
2018-07-11 10:46:04 +0800

    新增测试屏幕截图

---
+++ src/test/java/TestScreenCapture.java
@@ -0,0 +1,23 @@
+import com.tqhy.client.utils.FileUtils;
+import com.tqhy.client.utils.ViewsUtils;
+import org.junit.Test;
+
+/**
+ * @author Yiheng
+ * @create 2018/7/10
+ * @since 1.0.0
+ */
+public class TestScreenCapture {
+
+    @Test
+    public void testScreenCapture() {
+
+        String rootPath = FileUtils.getRootPath();
+        System.out.println("rootPath is: "+rootPath);
+        try {
+            ViewsUtils.captureScreen( "capture.png", rootPath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
