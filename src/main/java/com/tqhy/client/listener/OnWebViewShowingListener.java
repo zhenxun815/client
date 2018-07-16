5413294856e8522c905be9f543323900ebfc67f8
zhenxun815 <guoyiheng89@gmail.com>
2018-07-16 10:46:13 +0800

    新增WebView页面开启关闭监听

---
+++ src/main/java/com/tqhy/client/listener/OnWebViewShowingListener.java
@@ -0,0 +1,14 @@
+package com.tqhy.client.listener;
+
+import com.tqhy.client.controller.WebViewDialogController;
+
+/**
+ *
+ * 监听是否弹出webview窗体
+ * @author Yiheng
+ * @create 2018/7/16
+ * @since 1.0.0
+ */
+public interface OnWebViewShowingListener {
+    void shouldJnaFetchData(WebViewDialogController webViewDialogController);
+}
